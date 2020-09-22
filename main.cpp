51eb41da0d4417f2ae016b40721d1018d3e1a7af
jpfli <46481986+jpfli@users.noreply.github.com>
2020-09-22 19:32:07 +0300

    Moved Mixer::init() to Pomifactory class

--- main.cpp
+++ main.cpp
@@ -1,16 +1,10 @@
 
 #include "pomi/System.h"
-#include "pomi/Mixer.h"
 #include "Pomifactory.h"
 #include "StateMenu.h"
 
-using Pomi::System;
-using Pomi::Mixer;
-
 int main() {
-    System::init();
-    Mixer::init(11025);
-    Mixer::setVolume(128);
+    Pomi::System::init();
     
     Pomifactory::run(&StateMenu::instance());
 }
