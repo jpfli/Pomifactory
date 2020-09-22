7b6f0b7a4cf200a993097dd7d5fb3e42f23de88f
jpfli <46481986+jpfli@users.noreply.github.com>
2020-09-22 19:24:41 +0300

    Added drawImage1BPP()

--- pomi/Graphics.cpp
+++ pomi/Graphics.cpp
@@ -53,17 +53,15 @@ void Graphics::setClippingRegion(std::int32_t x, std::int32_t y, std::int32_t w,
 }
 
 void Graphics::clear(std::uint8_t color) {
-    setClippingRegion();
-    
     Display::beginStream();
     Display::writeData(_palette[color]);
-    for(uint32_t count = 1; count < width() * height(); ++count) {
+    for(std::uint32_t count = 1; count < width() * height(); ++count) {
         Display::writeData();
     }
 }
 
 void Graphics::drawPixel(std::int32_t x, std::int32_t y, std::uint8_t color) {
-    if((static_cast<uint32_t>(x) < width()) || (static_cast<uint32_t>(y) < height())) {
+    if((static_cast<std::uint32_t>(x) < width()) || (static_cast<std::uint32_t>(y) < height())) {
         Display::setY(y);
         Display::setX(x);
         
@@ -175,8 +173,7 @@ void Graphics::drawImage(const std::uint8_t* bitmap, std::int32_t x_src, std::in
         return; // Bitmap is off drawing area
     }
     
-    std::int32_t w_bmp = bitmap[0];
-    w_bmp += w_bmp & 1;
+    std::int32_t w_bmp = ~1 & (bitmap[0] + 1); // make width multiple of 2
     bitmap += 2; // Move to the start of pixel data
     
     // Vertically crop image to the clipping region
@@ -223,7 +220,7 @@ void Graphics::drawImage(const std::uint8_t* bitmap, std::int32_t x_src, std::in
         Display::beginStream();
         
         for(std::int32_t x_pos = x_src; x_pos != x_end; x_pos += x_inc) {
-            std::uint8_t color = bitmap[(y_pos * w_bmp + x_pos) / 2];
+            std::uint32_t color = bitmap[(y_pos * w_bmp + x_pos) / 2];
             color = (x_pos & 1) ? (color & 0xf) : (color >> 4);
             
             if(color != _transp_color) {
@@ -241,7 +238,7 @@ void Graphics::drawImage(const std::uint8_t* bitmap, std::int32_t x_src, std::in
             }
         }
     }
-
+    
 //     // This alternative loop works on emulator but not quite right on hw
 //     Display::setHorizontalRange(x_dst, x_dst + w - 1);
 //     Display::setY(y_dst);
@@ -275,6 +272,86 @@ void Graphics::drawImage(const std::uint8_t* bitmap, std::int32_t x_src, std::in
     
 //     Display::setHorizontalRange(0, width() - 1);
 // }
+}
+
+void Graphics::drawImage1BPP(const std::uint8_t* bitmap, std::uint8_t color, std::int32_t x_src, std::int32_t y_src, std::int32_t w, std::int32_t h, std::int32_t x_dst, std::int32_t y_dst, bool flip_x, bool flip_y) {
+    if((x_dst + w < _clip_x) || (x_dst >= _clip_x + _clip_w) || (y_dst + h < _clip_y )|| (y_dst >= _clip_y + _clip_h)) {
+        return; // Bitmap is off drawing area
+    }
+    
+    std::int32_t w_bmp = ~7 & (bitmap[0] + 7); // make width multiple of 8
+    bitmap += 2; // Move to the start of pixel data
+    
+    // Vertically crop image to the clipping region
+    if(y_dst < _clip_y) {
+        y_src = _clip_y - y_dst;
+        h -= _clip_y - y_dst;
+        y_dst = _clip_y;
+    }
+    if(y_dst + h >= _clip_y + _clip_h) {
+        h = _clip_y + _clip_h - y_dst;
+    }
+    
+    // Horizontally crop image to the clipping region
+    if(x_dst < _clip_x) {
+        x_src += _clip_x - x_dst;
+        w -= _clip_x - x_dst;
+        x_dst = _clip_x;
+    }
+    if(x_dst + w > _clip_x + _clip_w) {
+        w = _clip_x + _clip_w - x_dst;
+    }
+    
+    std::int8_t x_inc = 1;
+    std::int32_t x_end = x_src + w;
+    if(flip_x) {
+        x_inc = -1;
+        x_end = x_src - 1;
+        x_src = x_end + w;
+    }
+    
+    std::int8_t y_inc = 1;
+    std::int32_t y_end = y_src + h;
+    if(flip_y) {
+        y_inc = -1;
+        y_end = y_src - 1;
+        y_src = y_end + h;
+    }
+    
+    for(std::int32_t y_pos = y_src; y_pos != y_end; y_pos += y_inc) {
+        std::uint32_t skip = 0;
+        // Jump to the beginning of row
+        Display::setY(y_dst + (y_pos - y_src) * y_inc);
+        Display::setX(x_dst);
+        Display::beginStream();
+        
+        std::uint32_t bit_mask = 1 << (7 - x_src % 8);
+        
+        for(std::int32_t x_pos = x_src; x_pos != x_end; x_pos += x_inc) {
+            std::uint32_t pixels = bitmap[(y_pos * w_bmp + x_pos) / 8];
+            
+            if(pixels & bit_mask) {
+                if(skip > 0) {
+                    // Jump to current position on this row
+                    Display::setX(x_dst + (x_pos - x_src) * x_inc);
+                    Display::beginStream();
+                    skip = 0;
+                }
+                Display::writeData(_palette[color]);
+            }
+            else {
+                // Just ignore transparent pixels until next non-transparent pixel
+                ++skip;
+            }
+            
+            if(x_inc > 0) {
+                bit_mask = (bit_mask >> 1) ? (bit_mask >> 1) : (1 << 7); // 8 bit rotate right
+            }
+            else {
+                bit_mask = ((bit_mask << 1) >> 8) ? 1 : (bit_mask << 1); // 8 bit rotate left
+            }
+        }
+    }
 }
 
 void Graphics::print(const char* text, std::uint8_t color) {
