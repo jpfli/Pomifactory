3c640e90a873f14378d756490256884a380c987c
jpfli <46481986+jpfli@users.noreply.github.com>
2020-09-22 19:20:07 +0300

    Added check when SysTick counter has reset

--- pomi/System.h
+++ pomi/System.h
@@ -16,7 +16,20 @@ class System {
         
         static inline void init() { Hw::Core::enableSysTick(); }
         
-        static inline std::uint32_t currentTimeMillis() { return _uptime + Hw::Core::sysTickMillis(); }
+        static inline std::uint32_t currentTimeMillis() {
+            std::uint32_t uptime = _uptime;
+            std::uint32_t millis = Hw::Core::sysTickMillis();
+            
+            // If _uptime has changed, then SysTick counter has reset and should be read again
+            return uptime != _uptime ? (_uptime + Hw::Core::sysTickMillis()) : (uptime + millis);
+        }
+        static inline std::uint32_t currentTimeMicros() {
+            std::uint32_t uptime = _uptime;
+            std::uint32_t micros = Hw::Core::sysTickMicros();
+            
+            // If _uptime has changed, then SysTick counter has reset and should be read again
+            return uptime != _uptime ? (1000 * _uptime + Hw::Core::sysTickMicros()) : (1000 * uptime + micros);
+        }
         
         static inline void onSysTick() { _uptime += 100; }
         
