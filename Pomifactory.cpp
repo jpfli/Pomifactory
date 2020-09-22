b6fb5c9b4802749275d5d89038ffe47d489795d3
jpfli <46481986+jpfli@users.noreply.github.com>
2020-09-22 19:37:33 +0300

    Added loaderScreen()

--- Pomifactory.cpp
+++ Pomifactory.cpp
@@ -1,8 +1,11 @@
 
 #include "Pomifactory.h"
+#include "pomi/Mixer.h"
 #include "pomi/Graphics.h"
+#include "pomi/System.h"
 #include "pomi/Input.h"
 #include "pomi/Cookies.h"
+#include "pomi/hw/Eeprom.h"
 #include "StringBuffer.h"
 
 #include "GameInfo.h"
@@ -21,24 +24,22 @@
 #include "images/button_down.h"
 #include "images/cbtn_down.h"
 
+using Pomi::Mixer;
 using Pomi::Graphics;
 using Pomi::Input;
 using Button = Pomi::Input::Button;
+
+extern "C" void __wrap_exit(int);
 
 Graphics Pomifactory::_gfx(&_palette[0], lcdFont);
 
 std::uint32_t Pomifactory::_time_last_frame = 0;
 
 AssemblyBay Pomifactory::_asm_bay;
-
 Truck Pomifactory::_truck;
-
 Pomifactory::Smashes Pomifactory::_smashes = {_SMASH_SPRITES};
-
 GameInfo Pomifactory::_game_info(168, 18);
-
 Boss Pomifactory::_boss;
-
 Workers Pomifactory::_workers;
 
 Pomifactory::Cookie Pomifactory::_cookie;
@@ -67,6 +68,11 @@ void Pomifactory::reset(GameInfo::Mode mode) {
 }
 
 void Pomifactory::run(State* state) {
+    loaderScreen();
+    
+    Mixer::init(11025);
+    Mixer::setVolume(181);
+    
     changeState(state);
     
     while(true) {
@@ -82,7 +88,7 @@ void Pomifactory::run(State* state) {
         constexpr std::uint8_t FRAMES_PER_SECOND = 30;
         limitFPS(FRAMES_PER_SECOND);
         
-        Pomi::Input::update();
+        Pomi::Input::update(Pomi::System::currentTimeMillis());
         
         _cur_state->update();
     }
@@ -128,4 +134,49 @@ void Pomifactory::draw() {
     _truck.draw(_gfx, background);
     _asm_bay.draw(_gfx, background);
     _workers.draw(_gfx, background);
+}
+
+void Pomifactory::loaderScreen() {
+    _gfx.clear(12);
+    
+    _gfx.drawImage(player2, 82, 56);
+    _gfx.drawImage(item3, 106, 59);
+    _gfx.drawImage(player3, 115, 56, true);
+    
+    _gfx.print("Press   for loader", 47, 96, 3);
+    _gfx.print("C", 89, 96, 2);
+    
+    constexpr std::uint32_t LOADERWAIT = 4002;
+    std::int8_t wait = Pomi::Hw::Eeprom::read(LOADERWAIT);
+    if((wait <= 0) || (wait > 5)) wait = 3;
+    std::uint32_t exit_time = Pomi::System::currentTimeMillis() + 1000 * wait;
+    
+    std::int32_t counter = -1;
+    while(true) {
+        Input::update(0);
+        
+        if(Input::pressed(Input::Button::C)) {
+            __wrap_exit(0);
+        }
+        
+        std::int32_t counter_now = (exit_time - Pomi::System::currentTimeMillis() + 999) / 1000;
+        if(counter_now <= 0 || Input::pressed(Input::Button::A | Input::Button::B)) {
+            break;
+        }
+        else if(counter_now != counter) {
+            counter = counter_now;
+            
+            StringBuffer<5> text;
+            text.append(counter);
+            
+            std::int32_t x = 110 - 7 * text.length() / 2;
+            
+            _gfx.drawFilledRectangle(x, 120, 7 * text.length(), 11, 12);
+            _gfx.print(text, x, 120, 2);
+        }
+    }
+    
+    do {
+        Input::update(0);
+    } while(Input::held(Input::Button::A | Input::Button::B | Input::Button::C));
 }
